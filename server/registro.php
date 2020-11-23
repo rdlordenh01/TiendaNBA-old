6032d4bc87e9a9030547b61b8f3a2abdecbe4cdc
vagrant <vagrant@homestead>
2020-11-23 23:03:25 +0000

    Arreglado, sin Pedidos y sin Tarjetas

--- server/registro.php
+++ server/registro.php
@@ -5,12 +5,12 @@ include_once 'db.php';
 class Registro extends DB{
     
     function obtenerRegistros(){
-        $query = $this->connect()->query('SELECT r.tipo as tipoR, u.user as usuario, u.tipo as tipoU, r.fecha as fecha FROM registros r, usuarios u WHERE r.usuario=u.id ORDER BY r.id DESC');
+        $query = $this->connect()->query('SELECT r.tipo as tipoR, u.user as usuario, u.tipo as tipoU, r.created_at as fecha FROM registros r, users u WHERE r.usuario=u.id ORDER BY r.id DESC');
         return $query;
     }
 
     function obtenerRegistro($user,$orden){
-        $query = $this->connect()->prepare('SELECT r.tipo as tipoR, u.user as usuario, u.tipo as tipoU, r.fecha as fecha FROM registros r, usuarios u WHERE r.usuario=u.id AND usuario = :user ORDER BY r.id '.$orden);
+        $query = $this->connect()->prepare('SELECT r.tipo as tipoR, u.user as usuario, u.tipo as tipoU, r.created_at as fecha FROM registros r, users u WHERE r.usuario=u.id AND usuario = :user ORDER BY r.id '.$orden);
         $query->execute(['user' => $user]);
         return $query;
     }
@@ -21,7 +21,7 @@ class Registro extends DB{
     }
 
     function nuevoRegistro($registro,$max){
-        $query = $this->connect()->prepare('INSERT INTO registros (id,tipo,usuario,fecha) VALUES (:id,:tipo,:usuario,NOW())');
+        $query = $this->connect()->prepare('INSERT INTO registros (id,tipo,usuario,created_at) VALUES (:id,:tipo,:usuario,NOW())');
         $query->execute(['id' => $max['id']+1, 'tipo' => $registro['tipo'], 'usuario' => $registro['usuario']]);
         //print_r($query);
         return $query;
