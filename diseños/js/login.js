cb516eea8f866881c7ac6213367f046fd17add0d
Ruben <ruben_wada_79@hotmail.com>
2021-01-21 21:50:40 +0100

    prueba de conexión en login online 4

--- diseños/js/login.js
+++ diseños/js/login.js
@@ -25,10 +25,7 @@ function login(){
     //alert('../server/user.php?user='+document.getElementById("username").value+'&passwd='+document.getElementById("passwd").value);
     fetch ('http://nba-server.tk/user.php?user='+document.getElementById("username").value+'&passwd='+document.getElementById("passwd").value, {
         "method": "GET",
-        "headers": {
-            'Content-Type': 'application/json',
-            'API-Key': 'secret'
-          }
+        "origin": "http://nba-server.tk/user.php"
     })
         .then(data => data.json()) 
         .then(datos => {
